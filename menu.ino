34a06d8f177c7980d25c6393a6e9f130d49809b7
RuffaloLavoisier <jsh970307@daum.net>
2021-04-19 01:12:21 +0900

    [update] release -> main

--- menu.ino
+++ menu.ino
@@ -6,7 +6,8 @@ void menu() {
   display.setTextColor(WHITE);
   display.setCursor(centerX - 64, centerY - 32);
   for (int count = 0; count < 8; count++) {//draw 0-7 title
-    if (count == 4 && pointer_menu > 3) { //other page  count-4 and pointer menu > this is 3
+
+    if (count == 4 && pointer_menu > 3) { //other page  count-4 and pointer menu > this is 3 
       display.clearDisplay();
       display.setTextSize(1);
       display.setTextColor(WHITE);
@@ -15,10 +16,10 @@ void menu() {
 
     if (pointer_menu == count)//pointer and menu count match >> count : draw menu number , pointer menu : control user from button
     {
-      display.print(">");
+      display.print(">");//draw pointer " > "
       //------------------------------------------
       if (LOW == digitalRead(button_center)) {
-        Delay(100);
+        Delay(100);//non stop board .still running !
         switch (pointer_menu) { //select back
           case 0://back
             state = false;
@@ -33,11 +34,12 @@ void menu() {
             alarm_clock();
             break;
           case 4://select temp
-            temp_monitor();
+            temp_monitor(); //do you need this menu ?
             break;
-          case 5:
+          case 5://GPS
             break;
-          case 6:
+          case 6://BLUETOOTH
+            print_RSSI();
             break;
           case 7://select info
             show_info();
